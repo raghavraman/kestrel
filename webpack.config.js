d83248fac063e68a387dc92fafe315c7df56b77c
Raghav Raman <rraman@covermymeds.com>
2020-05-25 11:59:34 -0400

    added config files

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+var webpack = require('webpack')
+var path = require('path')
+var fileSystem = require('fs')
+var env = require('./utils/env')
+var CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var WriteFilePlugin = require('write-file-webpack-plugin')
+
+// load the secrets
+var alias = {}
+
+var secretsPath = path.join(__dirname, ('secrets.' + env.NODE_ENV + '.js'))
+
+var fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2', 'css']
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias.secrets = secretsPath
+}
+
+var options = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    popup: path.join(__dirname, 'src', 'js', 'popup.js'),
+    background: path.join(__dirname, 'src', 'js', 'background.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        loader: 'file-loader?name=[name].[ext]',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: alias
+  },
+  plugins: [
+    // clean the build folder
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new CopyWebpackPlugin([{
+      from: 'src/manifest.json',
+      transform: function (content, path) {
+        // generates the manifest file using the package.json informations
+        return Buffer.from(JSON.stringify({
+          description: process.env.npm_package_description,
+          version: process.env.npm_package_version,
+          ...JSON.parse(content.toString())
+        }))
+      }
+    }], { copyUnmodified: true }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new WriteFilePlugin({
+      // exclude hot-update files
+      test: /^(?!.*(hot)).*/
+    })
+  ]
+}
+
+if (env.NODE_ENV === 'development') {
+  options.devtool = 'cheap-module-eval-source-map'
+}
+
+module.exports = options
